e7fdcfd45ad7cdf6f71014704e5dda2fd7d5de56
urvashi29 <urvashigolu29@gmail.com>
2022-12-26 21:05:34 +0530

    added lec-13

---
+++ Lec-13/js/script.js
@@ -0,0 +1,43 @@
+// alert('you are successfully loggedin');
+// document.write("<h2>World</h2>"); //testing purpose
+// console.log('hello');//debugging purpose
+
+// variables
+var a = "sugar"; //ideal way
+var x = 20;
+
+var b; //declaration
+b = "salt"; //initialization / assigning a value
+
+// multiple variables in a single line
+var firstName = "alina",
+  age = 20;
+
+console.log(a, b);
+// alert(b);
+// document.write(a + b);
+document.getElementById("display-name").innerHTML = firstName;
+
+var gst = 20;
+var pp = 20000;
+// pp + gst;
+
+// var var = 90;//give error
+
+// Numbers
+var num = 90;
+var numTwo = 20.78;
+console.log(typeof num);
+console.log(typeof numTwo);
+
+console.log(typeof b);
+
+//
+// var name = prompt("Enter your name");
+// console.log(name);
+
+// var result = confirm("do you wish to continue");
+// console.log(result);
+
+// task: use external js
+// create a variable for a person in a single line(name, designation, age, salary, city) and display in UI and console
